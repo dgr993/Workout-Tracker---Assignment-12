611cd359d9fcfe4616338753b42fbc7d1d12b4b7
Wuzup <dgr993@gmail.com>
2020-09-25 20:09:57 -0600

    deploying

--- server.js
+++ server.js
@@ -14,10 +14,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.post("/submit", ({ body }, res) => {
   User.create(body)
